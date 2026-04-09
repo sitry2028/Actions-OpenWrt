47fe3a4e078df0d0156a472aeb1eed51c88dcd53
sitry2028 <sitry@163.com>
2026-04-10 02:23:17 +0800

    Update default IP and device model in config

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,12 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# 修改默认 IP 为 192.168.99.1
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
-
-# 修改设备型号为 TikTok-803（修改设备树中的 model 字段）
 sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTok-803"/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
-
-# 修改 UBI 分区大小为障眼法值（之前已添加，但确保存在）
-sed -i 's/0x580000 0x4000000/0x580000 0x20000000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
+sed -i 's/0x580000 0x4000000/0x580000 0x7280000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
 
