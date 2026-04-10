9247f700a6752de322f3d715e396f2adcbac088d
sitry2028 <sitry@163.com>
2026-04-10 22:34:16 +0800

    Update footer links in Luci HTML and Lua files
    Replace footer links from P3TERX to OpenWrt in Luci files.

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,6 @@
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTok-803"/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
 sed -i 's/0x580000 0x4000000/0x580000 0x7280000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
-
+# 替换页脚链接为 OpenWrt 官网
+find feeds/luci -type f -name "*.htm" -o -name "*.lua" | xargs sed -i 's|https://p3terx.com|https://openwrt.org|g'
+find feeds/luci -type f -name "*.htm" -o -name "*.lua" | xargs sed -i 's|P3TERX|OpenWrt|g'
