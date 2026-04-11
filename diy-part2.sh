0879cdfa78767566b19034d6d72d26fbd492ecf7
sitry2028 <sitry@163.com>
2026-04-11 23:25:04 +0800

    Fix UBI partition size in device model script

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,3 @@ sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTok-803"/' target/linux/mediatek/dts/mt7981b
 
 # Fix UBI partition size (use 0x7280000)
 sed -i 's/0x580000 0x4000000/0x580000 0x7280000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
-
-# 替换页脚链接为 OpenWrt 官网
-find feeds/luci -type f -name "*.htm" -o -name "*.lua" | xargs sed -i 's|https://p3terx.com|https://www.tiktiok.top|g'
-find feeds/luci -type f -name "*.htm" -o -name "*.lua" | xargs sed -i 's|P3TERX|OpenWrt|g'
