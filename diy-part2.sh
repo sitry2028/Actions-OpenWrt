92e55bd18879c1636ae158f5b0ef9c2b25bcc926
sitry2028 <sitry@163.com>
2026-04-11 23:12:13 +0800

    Modify network settings and device configuration
    Updated default IP and device model in configuration. Fixed UBI partition size.

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,22 +4,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+
+# Modify device model
 sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTok-803"/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
+
+# Fix UBI partition size (use 0x7280000)
 sed -i 's/0x580000 0x4000000/0x580000 0x7280000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
 
 # 替换页脚链接为 OpenWrt 官网
