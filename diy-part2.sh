85447299e165ac5f056809eb5d6c83588a86026d
sitry2028 <sitry@163.com>
2026-05-11 01:38:48 +0800

    Modify device model and update footer links
    Updated links and device model in the script.

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 # 修改设备型号
-sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTiok-803"/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
+sed -i 's/"Zbtlink ZBT-Z8103AX"/"TikTiok-803D"/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
 
 # 修正 UBI 分区大小
 sed -i 's/0x580000 0x4000000/0x580000 0x7280000/' target/linux/mediatek/dts/mt7981b-zbtlink-zbt-z8103ax.dts
@@ -38,11 +38,11 @@ chmod +x files/etc/uci-defaults/98-set-wifi-ssid
 ARGO_FOOTER="feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm"
 if [ -f "$ARGO_FOOTER" ]; then
     # 匹配包含 'class="luci-link"' 的行，将第一个 <a>...</a> 整体替换为自定义链接
-    sed -i '/class="luci-link"/ { s|<a class="luci-link"[^>]*>.*</a>|<a href="https://www.tiktiok.top/" target="_blank">TikTiok学堂免费资源</a>| }' "$ARGO_FOOTER"
+    sed -i '/class="luci-link"/ { s|<a class="luci-link"[^>]*>.*</a>|<a href="https://www.tiktiok.top/" target="_blank">TikTiok学堂 · 跨境电商免费资源</a>| }' "$ARGO_FOOTER"
 fi
 
 # 处理 bootstrap 主题（作为备用，如果存在）
 BOOT_FOOTER="feeds/luci/themes/luci-theme-bootstrap/luasrc/view/themes/bootstrap/footer.htm"
 if [ -f "$BOOT_FOOTER" ]; then
-    sed -i '/class="luci-link"/ { s|<a class="luci-link"[^>]*>.*</a>|<a href="https://www.tiktiok.top/" target="_blank">TikTiok学堂免费资源</a>| }' "$BOOT_FOOTER"
+    sed -i '/class="luci-link"/ { s|<a class="luci-link"[^>]*>.*</a>|<a href="https://www.tiktiok.top/" target="_blank">TikTiok学堂 · 跨境电商免费资源</a>| }' "$BOOT_FOOTER"
 fi
